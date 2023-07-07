c15075ca3b9d9e819d233d119b3f2c4f5067972c
adamjfox <adamjeffersonfox@gmail.com>
2023-07-07 13:34:00 -0700

    getting things labeled

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,3 +3,11 @@
 //add local storage
 //make function to add to local storage
 //add weatherforecast function
+
+var click = document.getElementById("search");
+var search = document.getElementById("cInput");
+var wc = document.getElementById("weather");
+var sh = document.getElementById("sh");
+var forecast = document.getElementById("wc");
+
+
